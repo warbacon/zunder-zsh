f2d880c2056382c0c7facf1750151ef0d503cd2d
Warbacon <joaquinguerratocino@gmail.com>
2022-05-05 13:25:08 +0200

    Beautified code

--- run.sh
+++ run.sh
@@ -21,39 +21,39 @@ distro_select() {
     echo -e "3. Fedora (dnf)"
     echo -e "4. Android (termux)\n"
     echo -e "Select your current distro [1-4]${NORMAL}"
-
+    
     read distro
 }
 
 install_zsh() {
     echo "--------------------------------------------------------------------"
     echo -e "${WARNING}Zsh is not installed, do you want to install it? [Y/n]${NORMAL}"
-
+    
     read prompt
-
+    
     echo ""
     case $prompt in
-    [nN])
-        echo -e "${ERROR}Zsh is needed to run the script.${NORMAL}"
-        exit
+        [nN])
+            echo -e "${ERROR}Zsh is needed to run the script.${NORMAL}"
+            exit
         ;;
-    *)
-        echo -e "Zsh will be installed.\n"
-
-        case $distro in
-        2)
-            sudo apt-get install zsh
-            ;;
-        3)
-            sudo dnf install zsh
-            ;;
-        4)
-            pkg install zsh
-            ;;
         *)
-            sudo pacman -S zsh
-            ;;
-        esac
+            echo -e "Zsh will be installed.\n"
+            
+            case $distro in
+                2)
+                    sudo apt-get install zsh
+                ;;
+                3)
+                    sudo dnf install zsh
+                ;;
+                4)
+                    pkg install zsh
+                ;;
+                *)
+                    sudo pacman -S zsh
+                ;;
+            esac
         ;;
     esac
 }
@@ -61,121 +61,59 @@ install_zsh() {
 zsh_default() {
     echo "--------------------------------------------------------------------"
     echo -e "${WARNING}Zsh is not your current defaut shell, do you want to set it? [Y/n]${NORMAL}"
-
+    
     read prompt
-
+    
     echo ""
     case $prompt in
-    [yY])
-        if [[ $distro = 4 ]]; then
-            chsh -s zsh
-        else
-            chsh -s $(which zsh)
-        fi
-        echo -e "${SUCCESS}Zsh was setted as the default shell, a reboot is needed to see the changes.${NORMAL}"
+        [yY])
+            if [[ $distro = 4 ]]; then
+                chsh -s zsh
+            else
+                chsh -s $(which zsh)
+            fi
+            echo -e "${SUCCESS}Zsh was setted as the default shell, a reboot is needed to see the changes.${NORMAL}"
         ;;
-    *)
-        echo -e "${WARNING}Zsh won't be setted as the default shell.${NORMAL}"
+        *)
+            echo -e "${WARNING}Zsh won't be setted as the default shell.${NORMAL}"
         ;;
     esac
 }
-
-# starhip_install() {
-#     echo "-------------------------------------------------------------------------"
-#     echo -e "${BOLD}Do you want to install the ${BLUEBOLD}Starship prompt${NORMAL}${BOLD} (${CYANBOLD}https://starship.rs${NORMAL}${BOLD})? [Y/n]${NORMAL}"
-# 
-#     read prompt
-# 
-#     echo ""
-#     case $prompt in
-#     [nN])
-#         echo -e "${WARNING}Starship won't be installed.${NORMAL}"
-#         ;;
-#     *)
-#         echo "Starhip will be instaled."
-#         if [[ $distro = 4 ]]; then
-#             pkg install starship
-#         else
-#             sh -c "$(curl -fsSL https://starship.rs/install.sh)"
-#         fi
-#         mkdir -p "$HOME"/.config && cp starship.toml "$HOME"/.config
-#         ;;
-#     esac
-# }
 
 exa_install() {
     echo "--------------------------------------------------------------------------------------------"
     echo -e "Exa is powerfull ls command replacement written in rust. It will show icons and colors for every file or directory.\n"
     echo -e "${BOLD}Do you want to install ${BLUEBOLD}exa${NORMAL}${BOLD}? [Y/n]${NORMAL}"
-
+    
     read prompt
-
+    
     echo ""
     case $prompt in
-    [nN])
-        echo -e "${WARNING}Exa won't be installed.${NORMAL}"
+        [nN])
+            echo -e "${WARNING}Exa won't be installed.${NORMAL}"
         ;;
-    *)
-        case $distro in
-        2)
-            echo -e "Exa will be installed.\n"
-            sudo apt install exa
-            ;;
-        3)
-            echo -e "Exa will be installed.\n"
-            sudo dnf install exa
-            ;;
-        4)
-            echo -e "Exa will be installed.\n"
-            pkg install exa
-            ;;
         *)
-            echo -e "Exa will be installed.\n"
-            sudo pacman -S exa
-            ;;
-        esac
+            case $distro in
+                2)
+                    echo -e "Exa will be installed.\n"
+                    sudo apt install exa
+                ;;
+                3)
+                    echo -e "Exa will be installed.\n"
+                    sudo dnf install exa
+                ;;
+                4)
+                    echo -e "Exa will be installed.\n"
+                    pkg install exa
+                ;;
+                *)
+                    echo -e "Exa will be installed.\n"
+                    sudo pacman -S exa
+                ;;
+            esac
         ;;
     esac
 }
-
-# lsd_install() {
-#     echo "--------------------------------------------------------------------------------------------"
-#     echo -e "Lsd is a beautified ls command. It will show icons and colors for every file or directory.\n"
-#     echo -e "${BOLD}Do you want to install ${BLUEBOLD}lsd${NORMAL}${BOLD}? [Y/n]${NORMAL}"
-# 
-#     read prompt
-# 
-#     echo ""
-#     case $prompt in
-#     [nN])
-#         lsd=0
-#         echo -e "${WARNING}Lsd won't be installed.${NORMAL}"
-#         ;;
-#     *)
-#         lsd=1
-#         case $distro in
-#         2)
-#             echo -e "${WARNING}Lsd isn't available in the Debian and Ubuntu repositories.${NORMAL}"
-#             echo -e "The binary file will be downloaded and installed.\n"
-#             wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb -O lsd.deb
-#             sudo dpkg -i lsd.deb
-#             ;;
-#         3)
-#             echo -e "Lsd will be installed.\n"
-#             sudo dnf install lsd
-#             ;;
-#         4)
-#             echo -e "Lsd will be installed.\n"
-#             pkg install lsd
-#             ;;
-#         *)
-#             echo -e "Lsd will be installed.\n"
-#             sudo pacman -S lsd
-#             ;;
-#         esac
-#         ;;
-#     esac
-# }
 
 install_zgenom() {
     echo "------------------------------------------------------------------------------------"
@@ -183,45 +121,22 @@ install_zgenom() {
     git clone https://github.com/jandamm/zgenom.git "${HOME}/.zgenom"
 }
 
-# install_plugins() {
-#     echo "-----------------------------------------------------------------------------"
-#     echo -e "${BLUEBOLD}fast-syntax-highlighting${NORMAL}${BOLD}, ${BLUEBOLD}zsh-autosuggestions${NORMAL}${BOLD} and ${BLUEBOLD}zsh-autopair${NORMAL}${BOLD} are three essential zsh plugins."
-#     echo -e "The plugins' repositories will be cloned at ${CYAN}$HOME/.config/zsh/plugins.${NORMAL}\n"
-#     echo -e "${BOLD}Do wou want to install the plugins? [Y/n]${NORMAL}"
-# 
-#     read prompt
-# 
-#     echo ""
-#     case $prompt in
-#     [nN])
-#         echo -e "${WARNING}The plugins won't be installed.${NORMAL}"
-#         ;;
-#     *)
-# 
-#         mkdir -p "$HOME"/.config/zsh/plugins
-#         git clone https://github.com/zsh-users/zsh-autosuggestions.git "$HOME"/.config/zsh/plugins/zsh-autosuggestions
-#         git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git "$HOME"/.config/zsh/plugins/fast-syntax-highlighting
-#         git clone https://github.com/hlissner/zsh-autopair.git "$HOME"/.config/zsh/plugins/zsh-autopair
-#         ;;
-#     esac
-# }
-
 load_zshrc() {
     echo "------------------------------------------------------------------------------------"
     echo -e "At last, we will load a custom .zshrc to your current user home directory (${CYAN}$HOME${NORMAL}).\n"
     echo -e "${WARNING}BACKUP YOUR .ZSHRC BEFORE CONTINUE AS IT WILL BE REPLACED.${NORMAL}\n"
     echo -e "${BOLD}Continue? [y/N]${NORMAL}"
-
+    
     read prompt
-
+    
     echo ""
     case $prompt in
-    [yY])
-        cp ./config/zshrc ~/.zshrc
-        cp ./config/p10k.zsh ~/.p10k.zsh
+        [yY])
+            cp ./config/zshrc ~/.zshrc
+            cp ./config/p10k.zsh ~/.p10k.zsh
         ;;
-    *)
-        echo -e "${WARNING}Canceled. This won't apply your changes at all, try running the script again.${NORMAL}"
+        *)
+            echo -e "${WARNING}Canceled. This won't apply your changes at all, try running the script again.${NORMAL}"
         ;;
     esac
 }
