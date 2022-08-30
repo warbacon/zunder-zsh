3faf7129d4b02caed34bbab5c52eee77932d2121
Warbacon <joaquinguerratocino@gmail.com>
2022-08-30 13:48:14 +0200

    fix fzf plugin history

--- remove.sh
+++ remove.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-source ./lib/output.sh
+source ./lib/color.sh
 
 # FUNCTIONS
+
+removeJunk() {
+    printf "${WARNING}Do you want to remove all the Zunder files? [y/N]: ${NORMAL}"
+    read prompt
+    if [[ $prompt = Y || $prompt = y  ]]; then
+        rm -rf "$HOME/.p10k.zsh" "$HOME/.local/share/zinit" "$HOME/.zshrc" "$HOME/.zcompinit"
+    fi
+}
 
 restoreBackup() {
     printf "${WARNING}Do you want to restore your backup file (${CYAN}$HOME/.zshrc.bak${WARNING})? [y/N]: ${NORMAL}"
     read prompt
     if [[ $prompt = Y || $prompt = y  ]]; then
         mv --verbose "$HOME/.zshrc.bak" "$HOME/.zshrc"
-    fi
-}
-
-removeJunk() {
-    printf "${WARNING}Do you want to remove all the Zunder files? [y/N]: ${NORMAL}"
-    read prompt
-    if [[ $prompt = Y || $prompt = y  ]]; then
-        rm -rf "$HOME/.p10k.zsh" "$HOME/.local/share/zinit" "$HOME/.zshrc"
     fi
 }
 
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source ./lib/output.sh
+source ./lib/color.sh
 
 # FUNCTIONS
 
