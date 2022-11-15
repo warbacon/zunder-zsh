21e4a4f589ec1077ebe9813192b5cb378228d4c4
Warbacon <joaquinguerratocino@gmail.com>
2022-11-15 16:05:21 +0100

    Add more informative completions

--- config/options.zsh
+++ config/options.zsh
@@ -4,15 +4,17 @@ HISTORY_IGNORE='(q|exit|clear|clr)'
 HISTSIZE=10000
 SAVEHIST=10000
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
-setopt hist_ignore_dups       # ignore duplicated commands history list
 setopt hist_ignore_space      # ignore commands that start with space
-setopt hist_verify            # show command with history expansion to user before running it
 setopt share_history          # share command history data
 
 # COMPLETION STYLE -----------------------------------------------------------------------
 zstyle ':completion:*' completer _extensions _complete _approximate
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}'
+zstyle ':completion:*:*:*:*:descriptions' format '%F{green}-- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}no matches found%f'
+zstyle ':completion:*' use-cache yes
+zstyle ':completion:*' cache-path $ZSH_CACHE_DIR
 
 # OTHER ----------------------------------------------------------------------------------
 setopt autocd     # change working directory without using cd
