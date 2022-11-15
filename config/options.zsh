5cb33ccfcc8225e92d46e6ec9c298c780c912250
Warbacon <joaquinguerratocino@gmail.com>
2022-11-15 14:01:05 +0100

    Better history

--- config/options.zsh
+++ config/options.zsh
@@ -3,7 +3,11 @@ HISTFILE="$ZDOTDIR/.zsh_history"
 HISTORY_IGNORE='(q|exit|clear|clr)'
 HISTSIZE=10000
 SAVEHIST=10000
-setopt SHARE_HISTORY
+setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # ignore duplicated commands history list
+setopt hist_ignore_space      # ignore commands that start with space
+setopt hist_verify            # show command with history expansion to user before running it
+setopt share_history          # share command history data
 
 # COMPLETION STYLE -----------------------------------------------------------------------
 zstyle ':completion:*' completer _extensions _complete _approximate
