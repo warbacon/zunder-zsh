54db1bb6c3436114511ddd8b8803969374a557fe
Warbacon <joaquinguerratocino@gmail.com>
2023-02-19 21:55:10 +0100

    Change method of caching fnm start

--- config/plugins.zsh
+++ config/plugins.zsh
@@ -10,7 +10,6 @@ source "${ZINIT_HOME}/zinit.zsh"
 # PLUGIN CONFIGURATIONS ------------------------------------------------------------------
 ZSH_AUTOSUGGEST_MANUAL_REBIND=false
 WD_CONFIG="$ZDOTDIR/.warprc"
-ZSH_EVALCACHE_DIR="$ZDOTDIR/.zsh-evalcache"
 
 # PLUGINS --------------------------------------------------------------------------------
 zinit ice depth"1"
@@ -23,6 +22,12 @@ zi ice from"gh-r" as"program" pick"bin/exa"
 zi light ogham/exa
 zi ice as"completion" 
 zi snippet "$ZINIT_PLUGINS/ogham---exa/completions/exa.zsh"
+
+zi ice from"gh-r" as"program"
+zi light "Schniz/fnm"
+zinit light NICHOLAS85/z-a-eval
+zi ice eval"fnm env" atclone"fnm completions >> $ZINIT[COMPLETIONS_DIR]/_fnm"
+zi light "zdharma-continuum/null"
 
 zi ice wait lucid
 zi light hlissner/zsh-autopair
@@ -44,13 +49,3 @@ zi wait lucid light-mode for \
     blockf atpull'zinit creinstall -q .' \
         zsh-users/zsh-completions
 
-# FNM INSTALLATION -----------------------------------------------------------------------
-zi ice from"gh-r" as"program"
-zi light "Schniz/fnm"
-
-if [[ ! -f "$ZINIT[COMPLETIONS_DIR]/_fnm" ]]; then
-    echo "$(fnm completions)" >> "$ZINIT[COMPLETIONS_DIR]/_fnm"
-fi
-
-zi light "mroth/evalcache"
-_evalcache fnm env
