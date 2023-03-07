61b29e4ea67dd690318198e5c71e2a8298461689
Warbacon <joaquinguerratocino@gmail.com>
2023-03-07 21:07:55 +0100

    feat(plugins): faster load

--- config/plugins.zsh
+++ config/plugins.zsh
@@ -14,7 +14,7 @@ zi light "junegunn/fzf"
 
 zi ice from"gh-r" as"program" pick"fd*/fd" if'[[ $(uname -m) = "x86_64" ]]' \
     atload"FZF_ALT_C_COMMAND=\"fd -H --exclude=.git --type=directory\";
-           FZF_CTRL_T_COMMAND=\"fd -H --exclude=.git\""
+    FZF_CTRL_T_COMMAND=\"fd -H --exclude=.git\""
 zi light "sharkdp/fd"
 
 zi ice from"gh-r" as"program" pick"bin/exa" if'[[ $(uname -m) = "x86_64" ]]' has"unzip"
@@ -22,21 +22,20 @@ zi light "ogham/exa"
 zi ice as"completion" has"exa"
 zi snippet "https://raw.githubusercontent.com/ogham/exa/master/completions/zsh/_exa"
 
-zi ice wait lucid
-zi light "hlissner/zsh-autopair"
+zi wait lucid light-mode for \
+    "hlissner/zsh-autopair" \
+    "https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh" \
+    OMZP::command-not-found \
+    OMZP::sudo
 
-zi snippet OMZP::command-not-found
-zi snippet OMZP::sudo
 zi snippet OMZL::key-bindings.zsh
-zi snippet "https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.zsh"
 
 zi wait lucid light-mode for \
-    atinit"zicompinit; zicdreplay" \
+    atinit"ZINIT[COMPINIT_OPTS]=-C; zpcompinit; zpcdreplay" \
         zdharma-continuum/fast-syntax-highlighting \
     atload"ZSH_AUTOSUGGEST_MANUAL_REBIND=false;
-           ZSH_AUTOSUGGEST_STRATEGY=(history completion);
-           _zsh_autosuggest_start" \
+    ZSH_AUTOSUGGEST_STRATEGY=(history completion);
+    _zsh_autosuggest_start" \
         zsh-users/zsh-autosuggestions \
-    blockf atpull'zinit creinstall -q .' \
+    as"completion" \
         zsh-users/zsh-completions
-
