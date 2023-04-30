3a9bc513cf74a5f2fe19dfe03d8e93bddfb84d24
Warbacon <joaquinguerratocino@gmail.com>
2023-04-30 22:08:29 +0200

    utils: update install and uninstall scripts

--- uninstall.sh
+++ uninstall.sh
@@ -11,7 +11,7 @@ main() {
     if [[ $prompt =~ (Y|y) ]]; then
         mv "$ZDOTDIR/.zsh_history" "$HOME" 2> /dev/null
         rm -rf "$ZDOTDIR" "$HOME/.zshenv" "$HOME/.cache/p10k"* "$HOME/.cache/zinit" \
-               "$HOME/.local/share/zinit" "$HOME/.cache/starship"
+               "$HOME/.local/share/zinit" "$HOME/.cache/starship" "$HOME/.cache/gitstatus"
     fi
     print_line
     print_success "All done."
