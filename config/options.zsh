bab605beded72e59db669dd10d4c25d121d05489
Warbacon <joaquinguerratocino@gmail.com>
2023-08-20 17:59:47 +0200

    change: improve default options

--- config/options.zsh
+++ config/options.zsh
@@ -2,30 +2,40 @@
 HISTFILE="$ZDOTDIR/.zsh_history"
 HISTSIZE=50000
 SAVEHIST=10000
-setopt INC_APPEND_HISTORY        # Share history between all sessions.
-setopt HIST_IGNORE_SPACE         # Do not store commands prefixed with a space
+setopt hist_expire_dups_first # Delete duplicates first when HISTFILE size exceeds HISTSIZE
+setopt hist_ignore_dups       # Ignore duplicated commands history list
+setopt hist_ignore_space      # Do not store commands prefixed with a space
+setopt hist_verify            # Show command with history expansion to user before running it
+setopt share_history          # Share history between all sessions.
 
 # COMPLETION STYLE -------------------------------------------------------------
-setopt GLOBDOTS 
-zstyle ':completion:*' menu select
+WORDCHARS=''
+unsetopt menu_complete
+unsetopt flowcontrol
+setopt auto_menu
+setopt complete_in_word
+setopt always_to_end
 zstyle ':completion:*' completer _complete _match _approximate
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*' menu select
 zstyle ':completion:*' rehash true
+zstyle ':completion:*' special-dirs true
+zstyle ':completion:*' use-cache yes
+zstyle ':completion:*' cache-path $ZSH_CACHE_DIR
+autoload -U +X bashcompinit && bashcompinit
 
 # OTHER ------------------------------------------------------------------------
-setopt AUTOCD     # Change working directory without using cd
+setopt autocd # Change working directory without using cd
 
 # Change window title
-# It doesn't change it when shell_integration is enabled in kitty terminal
-# because it has its own way of doing it
+## It doesn't change it when shell_integration is enabled in kitty terminal
+## because it has its own way of doing it.
 if [[ -z $KITTY_SHELL_INTEGRATION && $TERM != "linux" ]]; then
-    function set_win_title(){
-        printf "\033]0;%s@%s:%s\007" "${USER}" "${HOST%%.*}" "${PWD/#$HOME/~}"
-    }
-    precmd_functions+=(set_win_title)
+  function set_win_title() {
+    printf "\033]0;%s@%s:%s\007" "${USER}" "${HOST%%.*}" "${PWD/#$HOME/~}"
+  }
+  precmd_functions+=(set_win_title)
 fi
 
-zle_highlight+=(paste:none)  # Disable background color on paste
-
-autoload -Uz select-word-style && select-word-style bash  # Bash word style
-
+# Disable background color on paste
+zle_highlight+=(paste:none)
