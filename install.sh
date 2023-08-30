3385aca33699fed093de3dbc299a4e295b29ad81
Warbacon <joaquinguerratocino@gmail.com>
2023-08-30 15:58:29 +0200

    Replace the old installer with the new one

--- install.sh
+++ install.sh
@@ -1,203 +1,260 @@
-#!/bin/bash
+#!/bin/sh
 
-source ./lib/color.sh
-source ./lib/functions.sh
-source ./lib/constants.sh
+FMT_RED=$(printf "\033[31m")
+FMT_GREEN=$(printf "\033[32m")
+FMT_YELLOW=$(printf "\033[33m")
+FMT_BLUE=$(printf "\033[34m")
+FMT_CYAN=$(printf "\033[36m")
+FMT_BOLD=$(printf "\033[1m")
+FMT_RESET=$(printf "\033[0m")
 
-select_system() {
-    local distro_name
-        
-    printf "\n%bWelcome to the %bzunder-zsh%b installation script.%b\n" \
-           "$ITALIC" "$YELLOW" "$NORMAL$ITALIC" "$NORMAL"
-    print_line
-    echo "1. Arch based (pacman)."
-    echo "2. Debian/Ubuntu based (apt)."
-    echo "3. Fedora based (dnf)."
-    echo "4. Void Linux."
-    echo "5. Mac OS."
-    echo "6. Android."
-    echo "7. Other."
-    read -rp $'\nSelect your current operating system [1-7]: ' distro
+ZDOTDIR="$HOME/.config/zsh"
 
-    while ! [[ "$distro" =~ ^[1-7]$ ]]; do
-        print_error "Value entered is invalid."
-        read -rp $'\nSelect your current operating system [1-7]: ' distro
-    done
+set -e
 
-    case $distro in
-        1) distro_name="Arch based";;
-        2) distro_name="Debian/Ubuntu based";;
-        3) distro_name="Fedora based";;
-        4) distro_name="Void Linux";;
-        5) distro_name="Mac OS";;
-        6) distro_name="Android";;
-        7) distro_name="Other";;
-        *) distro_name="an unknown distro";;
-    esac
-    print_info "You entered $distro_name."
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
 }
 
-dependecy_check() {
-    declare -a dependencies=("zsh" "unzip" "curl" "git")
-
-    print_line
-    echo "DEPENDENCY CHECK"
-    print_line
-
-    if (( distro == 3 )); then
-        dependencies+=("sqlite3")
-    fi
-
-    if (( distro == 6 )); then
-        dependencies+=("exa" "file")
-    fi
-
-    not_installed=0
-    for dependency in "${dependencies[@]}"; do
-        if ! command_exists "$dependency"; then
-            if (( distro == 7 )); then
-                print_warning "$dependency is not installed and is required." \
-                              "Please install it manually and try again."
-                exit 1
-            elif [[ "$dependency" == "sqlite3" ]]; then
-                install_program sqlite
-            else
-                install_program "$dependency"
-            fi
-            (( not_installed++ ))
-        fi
-    done
-
-    if (( not_installed == 0 )); then
-        print_success "All dependencies are satisfied."
-    fi
+fmt_error() {
+  printf "%sError: %s%s\n" "${FMT_BOLD}${FMT_RED}" "$*" "$FMT_RESET" >&2
 }
 
-install_program() {
-    local prompt
-
-    print_warning "$1 is not installed and is required."
-    read -rp $'\nInstall it? [Y/n]: ' prompt
-
-    prompt=${prompt:-Y}
-
-    if [[ "$prompt" =~ [Nn] ]]; then
-        print_error "$1 is required."
-        exit 1
-    fi
-
-    printf "\n"
-    case $distro in
-        1) sudo pacman -S "$1" ;;
-        2) sudo apt install "$1" ;;
-        3) sudo dnf install "$1" ;;
-        4) sudo xbps-install "$1" ;;
-        5) brew install "$1" ;;
-        6) pkg install "$1" ;;
-        *) print_error "An error has occurred." ;;
-    esac
+fmt_warning() {
+  printf "%s%s%s\n" "${FMT_BOLD}${FMT_YELLOW}" "$*" "$FMT_RESET"
 }
 
-set_default() {
-    local prompt
-
-    print_line
-    read -rp "Zsh is not your current default shell, do you want to set it? [Y/n]: " prompt
-
-    case $prompt in
-        [Nn])
-            print_warning "Zsh won't be setted as the default shell."
-            ;;
-        *)
-            if (( distro != 6 )); then
-                sudo usermod -s "$(which zsh)" "$USER" && default_applied=true
-            else
-                chsh -s zsh && default_applied=true
-            fi
-
-            if $default_applied; then
-                print_success "Zsh was setted as the default shell."
-            else
-                print_warning "Zsh was not setted as the default shell."
-            fi
-            ;;
-    esac
+fmt_success() {
+  printf "%s%s%s\n" "$FMT_GREEN" "$*" "$FMT_RESET"
 }
 
-load_files() {
-    local prompt
+fmt_info() {
+  printf "%s:: %s%s%s\n" "${FMT_BOLD}${FMT_CYAN}" "${FMT_RESET}${FMT_BOLD}" "$*" "$FMT_RESET"
+}
 
-    print_line
-    printf "Zunder-zsh will store its configuration in %b%s%b.\n" \
-           "$CYAN" "$ZDOTDIR" "$NORMAL"
-    read -rp "Continue? [y/N]: " prompt
+fmt_prompt() {
+  printf "%s:: %s%s%s" "${FMT_BOLD}${FMT_BLUE}" "${FMT_RESET}${FMT_BOLD}" "$*" "$FMT_RESET"
+}
 
-    case $prompt in
-        [yY])
-            printf "\n"
-            mkdir -vp "$ZDOTDIR" 2> /dev/null
-            cp -v ./config/aliases.zsh "$ZDOTDIR"
-            cp -v ./config/options.zsh "$ZDOTDIR"
-            cp -v ./config/key-bindings.zsh "$ZDOTDIR"
-            cp -v ./config/plugins.zsh "$ZDOTDIR"
-            cp -v ./config/.zshrc "$ZDOTDIR"
-            cp -v ./config/.zshenv "$HOME"
-            mv -v "$HOME/.zsh_history" "$ZDOTDIR" 2> /dev/null
-            [ ! -f "$ZDOTDIR/user-config.zsh" ] && \
-                echo "# Write your configurations here" > "$ZDOTDIR/user-config.zsh"
-            ;;
-        *)
-            print_error "Canceled. This won't apply your changes at all," \
-                        "try running the script again."
-            exit 1
-            ;;
-    esac
+print_line() {
+  printf "%*s\n" "$(stty size | cut -d ' ' -f2)" '' | tr ' ' '-'
+}
+
+user_can_sudo() {
+  # Check if sudo is installed
+  command_exists sudo || return 1
+  # Termux can't run sudo, so we can detect it and exit the function early.
+  [ "$os_type" = "android" ] && return 1
+  # The following command has 3 parts:
+  #
+  # 1. Run `sudo` with `-v`. Does the following:
+  #    • with privilege: asks for a password immediately.
+  #    • without privilege: exits with error code 1 and prints the message:
+  #      Sorry, user <username> may not run sudo on <hostname>
+  #
+  # 2. Pass `-n` to `sudo` to tell it to not ask for a password. If the
+  #    password is not required, the command will finish with exit code 0.
+  #    If one is required, sudo will exit with error code 1 and print the
+  #    message:
+  #    sudo: a password is required
+  #
+  # 3. Check for the words "may not run sudo" in the output to really tell
+  #    whether the user has privileges or not. For that we have to make sure
+  #    to run `sudo` in the default locale (with `LANG=`) so that the message
+  #    stays consistent regardless of the user's locale.
+  #
+  ! LANG='' sudo -n -v 2>&1 | grep -q "may not run sudo"
 }
 
 install_icons() {
-    print_info "Installing icons..."
+  fmt_info "Installing icons..."
 
-    # Create directory if it does not exist
-    mkdir -p "$HOME/.local/share/fonts"
+  # Create directory if it does not exist
+  mkdir -p "$HOME/.local/share/fonts"
 
-    # Download font file
-    curl -fLo "$HOME/.local/share/fonts/Symbols Nerd Font.ttf" \
-        "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/NerdFontsSymbolsOnly/SymbolsNerdFont-Regular.ttf" \
-        || return 1
+  # Download font file
+  curl -fLo "$HOME/.local/share/fonts/Symbols Nerd Font.ttf" \
+    "https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/NerdFontsSymbolsOnly/SymbolsNerdFont-Regular.ttf" \
+    || return 1
+}
 
-    print_success "Icons installed successfully in $HOME/.local/share/fonts/Symbols Nerd Font.tff."
+install_package() {
+  fmt_prompt "Do you want to install $*? [Y/n] "
+  read -r response
+  if [ "$response" != "n" ] && [ "$response" != "N" ]; then
+    case $os_type in
+      "arch") sudo pacman -S --noconfirm "$*" ;;
+      "debian") sudo apt install -y "$*" ;;
+      "fedora") sudo dnf install --assumeyes "$*" ;;
+      "darwin") brew install "$*" ;;
+      "android") pkg install -y "$*" ;;
+      "void") sudo xbps-install -y "$*" ;;
+      *)
+        echo
+        fmt_error "Zunder-zsh doesn't support automatic package installations" \
+          "in your current operating system."
+        echo "Please do it manually."
+        exit 1
+        ;;
+    esac || {
+      fmt_error "Installation failed."
+      exit 1
+    }
+  else
+    return 1
+  fi
+}
+
+check_os_type() {
+  if [ -f /etc/os-release ]; then
+    os_type=$(grep "^ID_LIKE=" /etc/os-release | cut -d "=" -f 2 | tr -d '"')
+    if [ -z "$os_type" ]; then
+      os_type=$(grep "^ID=" /etc/os-release | cut -d "=" -f 2 | tr -d '"')
+    fi
+  else
+    [ "$(uname)" = "Darwin" ] && os_type="darwin"
+    case "$PREFIX" in
+      *com.termux*) os_type="android" ;;
+    esac
+  fi
+  case $os_type in
+    "arch")
+      fmt_info "You are using an arch-like distro."
+      ;;
+    "debian")
+      fmt_info "You are using a debian-like distro."
+      ;;
+    "fedora")
+      fmt_info "You are using a fedora-like distro."
+      ;;
+    "darwin")
+      fmt_info "You are using MacOS."
+      ;;
+    "android")
+      fmt_info "You are using termux on Android."
+      ;;
+    "void")
+      fmt_info "You are using Void Linux."
+      ;;
+    *)
+      fmt_warning "The type of your operating system could not be detected."
+      echo "The functionality of the installer will be limited."
+      echo
+      fmt_prompt "Continue? [Y/n] "
+      read -r response
+      if [ "$response" = "n" ] || [ "$response" = "N" ]; then
+        exit 1
+      fi
+      os_type="unknown"
+      ;;
+  esac
+}
+
+check_dependencies() {
+  set -- "zsh" "unzip" "curl" "git"
+
+  [ "$os_type" = "fedora" ] && set -- "$@" "sqlite"
+
+  fmt_info "Checking dependencies..."
+
+  for dependency in "$@"; do
+    if ! command_exists "$dependency"; then
+
+      [ "$dependency" = "sqlite" ] && dependency="sqlite3"
+
+      if [ "$os_type" = "unknown" ]; then
+        fmt_error "$dependency is needed for zunder-zsh to work properly."
+        echo "Please install it manually."
+        exit 1
+      fi
+
+      install_package "$dependency" || {
+        fmt_error "$dependency is needed for zunder-zsh to work properly."
+        echo "Please install it manually or run again this script."
+        exit 1
+      }
+    fi
+
+    shift 1
+
+  done
+
+  [ "$#" -eq 0 ] && fmt_success "All dependencies satisfied."
+}
+
+load_files() {
+  printf "%s::%s Zunder-zsh will store its configuration in %s.\n" \
+    "${FMT_BOLD}${FMT_CYAN}" "${FMT_RESET}${FMT_BOLD}" \
+    "${FMT_CYAN}${ZDOTDIR}${FMT_RESET}"
+  fmt_prompt "Continue? [y/N]: "
+  read -r prompt
+
+  echo
+  if [ "$prompt" = "y" ] || [ "$prompt" = "Y" ]; then
+    [ ! -d "$ZDOTDIR" ] && mkdir -vp "$ZDOTDIR"
+    cp -v ./config/aliases.zsh "$ZDOTDIR"
+    cp -v ./config/options.zsh "$ZDOTDIR"
+    cp -v ./config/key-bindings.zsh "$ZDOTDIR"
+    cp -v ./config/plugins.zsh "$ZDOTDIR"
+    cp -v ./config/.zshrc "$ZDOTDIR"
+    cp -v ./config/.zshenv "$HOME"
+    [ -f "$HOME/.zsh_history" ] \
+      && [ -f "$ZDOTDIR/.zsh_history" ] \
+      && [ -n "$(find "$HOME/.zsh_history" -newer "$ZDOTDIR/.zsh_history")" ] \
+      && mv -v "$HOME/.zsh_history" "$ZDOTDIR"
+    [ ! -f "$ZDOTDIR/user-config.zsh" ] \
+      && echo "# Write your configurations here" >"$ZDOTDIR/user-config.zsh"
+    echo
+    fmt_success "The configuration has been set up."
+  else
+    fmt_warning "Canceled. This won't apply your changes at all," \
+      "try running the script again."
+    exit 1
+  fi
+}
+
+set_default() {
+  if [ "$os_type" != "android" ]; then
+    sudo usermod -s "$(which zsh)" "$USER"
+  else
+    chsh -s zsh
+  fi
 }
 
 main() {
-    declare -i distro
-    default_applied=false
+  check_os_type
+  echo
+  check_dependencies
+  echo
+  load_files
+  if ! [ -d "$HOME/.local/share/zinit" ]; then
+    echo
+    fmt_info "Installing plugins..."
+    zsh -i -c exit
+  fi
+  if [ "$os_type" != "darwin" ] && [ "$os_type" != "android" ] && [ "$os_type" != "unknown" ]; then
+    fc-list | grep -q "Symbols Nerd Font" || (echo && install_icons)
+  fi
 
-    select_system
+  if [ "$(basename "$SHELL")" = "zsh" ]; then
+    echo
+    fmt_prompt "Zsh is not your current default shell, do you want to set it? [Y/n]: "
+    read -r prompt
 
-    dependecy_check
-
-    if [[ "$(basename "$SHELL")" != "zsh" && $distro -ne 5 && $distro -ne 7 ]]; then
-        set_default
+    if [ "$prompt" != "n" ] && [ "$prompt" != "N" ]; then
+      if set_default >/dev/null 2>&1; then
+        fmt_success "Zsh was applied as default shell."
+        fmt_warning "You may have to restart your computer to apply the changes."
+      else
+        fmt_error "Zsh could not be applied as the default shell."
+      fi
+    else
+      fmt_warning "Zsh won't be setted as the default shell."
     fi
+  fi
 
-    load_files
-
-    if [[ ! -d "$HOME/.local/share/zinit" ]]; then
-        print_info "Installing zunder-zsh..."
-        zsh -i -c exit
-    fi
-
-    if [[ "$distro" -lt 5  ]]; then
-        fc-list | grep -q "Symbols Nerd Font" || install_icons
-    fi
-
-    print_line
-    print_success "All done."
-
-    if [[ -n "$default_applied" ]]; then
-        print_warning "It may be necessary to reboot the system to see" \
-                      "zsh as the default shell."
-    fi
+  echo
+  fmt_success "Installation complete."
 }
 
 main "$@"
