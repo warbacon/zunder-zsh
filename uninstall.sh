dd7cbca178c148023bb82453e5f745040c1e62d1
Warbacon <joaquinguerratocino@gmail.com>
2023-09-03 13:39:46 +0200

    fix: uninstaller not doing anything

--- uninstall.sh
+++ uninstall.sh
@@ -7,7 +7,7 @@ set -e
 main() {
   printf "Do you want to remove all the Zunder files? [y/N]: "
   read -r prompt
-  if [ "$prompt" = "y" ] && [ "$prompt" = "Y" ]; then
+  if [ "$prompt" = "y" ] || [ "$prompt" = "Y" ]; then
     [ -f "$HOME/.zsh_history" ] && mv "$HOME/.zsh_history" "$HOME/.zsh_history.old"
     [ -f "$ZDOTDIR/.zsh_history" ] && mv "$ZDOTDIR/.zsh_history" "$HOME"
     rm -rf "$ZDOTDIR" "$HOME/.zshenv" "$HOME/.cache/p10k"* \
