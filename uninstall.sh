6aabf7aa385b8ecefd85e265f1205d06339647bb
Warbacon <joaquinguerratocino@gmail.com>
2023-09-03 12:48:36 +0200

    Reformat uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -5,15 +5,15 @@ ZDOTDIR="$HOME/.config/zsh"
 set -e
 
 main() {
-    printf "Do you want to remove all the Zunder files? [y/N]: "
-    read -r prompt
-    if [ "$prompt" = "y" ] && [ "$prompt" = "Y" ]; then
-        [ -f "$HOME/.zsh_history" ] && mv "$HOME/.zsh_history" "$HOME/.zsh_history.old"
-        [ -f "$ZDOTDIR/.zsh_history" ] && mv "$ZDOTDIR/.zsh_history" "$HOME"
-        rm -rf "$ZDOTDIR" "$HOME/.zshenv" "$HOME/.cache/p10k"* \
-            "$HOME/.cache/zinit" "$HOME/.local/share/zinit" \
-            "$HOME/.cache/starship" "$HOME/.cache/gitstatus"
-    fi
+  printf "Do you want to remove all the Zunder files? [y/N]: "
+  read -r prompt
+  if [ "$prompt" = "y" ] && [ "$prompt" = "Y" ]; then
+    [ -f "$HOME/.zsh_history" ] && mv "$HOME/.zsh_history" "$HOME/.zsh_history.old"
+    [ -f "$ZDOTDIR/.zsh_history" ] && mv "$ZDOTDIR/.zsh_history" "$HOME"
+    rm -rf "$ZDOTDIR" "$HOME/.zshenv" "$HOME/.cache/p10k"* \
+      "$HOME/.cache/zinit" "$HOME/.local/share/zinit" \
+      "$HOME/.cache/starship" "$HOME/.cache/gitstatus"
+  fi
 }
 
 main "$@"
