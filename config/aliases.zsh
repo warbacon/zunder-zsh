50b8c0528d40270175e8f18be57c0eb1d9b0507a
Warbacon <joaquinguerratocino@gmail.com>
2023-10-10 08:39:09 +0200

    feat: add lt alias and supports eza

--- config/aliases.zsh
+++ config/aliases.zsh
@@ -1,18 +1,27 @@
 # ALIASES ----------------------------------------------------------------------
 alias grep="grep --color=auto"
-if [[ -n $commands[exa] ]]; then
-  if [[ $TERM != "linux" ]]; then
-    alias exa="exa --icons --group-directories-first"
+
+[[ -n $commands[eza] && -z $commands[exa] ]] && alias exa="eza"
+
+if [[ -n $commands[exa] ]] || alias exa 2>&1 /dev/null; then
+  if [[ "$TERM" != "linux" ]]; then
+    alias ls="exa --icons --group-directories-first"
+    alias ls="exa --icons --group-directories-first"
+    alias ll="exa --icons --group-directories-first --git -lh"
+    alias la="exa --icons --group-directories-first -a"
+    alias lla="exa --icons --group-directories-first --git -lah"
+    alias lt="exa --icons -T"
   else
-    alias exa="exa --group-directories-first"
+    alias ls="exa --group-directories-first"
+    alias ll="exa --group-directories-first --git -lh"
+    alias la="exa --group-directories-first -a"
+    alias lla="exa --group-directories-first --git -lah"
+    alias lt="exa -T"
   fi
-  alias ls="exa"
-  alias ll="exa --git -lh"
-  alias la="exa -a"
-  alias lla="exa --git -lah"
 else
   alias ls="ls --color=auto --group-directories-first"
   alias ll="ls -lh"
   alias la="ls -A"
   alias lla="ls -lAh"
+  [[ -n $commands[tree] ]] && alias lt="tree ."
 fi
