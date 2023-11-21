44be8ddf1f09d70e8cef0a50b521dbce9330a6f1
Warbacon <joaquinguerratocino@gmail.com>
2023-11-21 14:00:56 +0100

    Update uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
-ZDOTDIR="$HOME/.config/zsh"
-
 set -e
 
 main() {
   printf "Do you want to remove all the Zunder files? [y/N]: "
   read -r prompt
   if [ "$prompt" = "y" ] || [ "$prompt" = "Y" ]; then
-    [ -f "$HOME/.zsh_history" ] && mv "$HOME/.zsh_history" "$HOME/.zsh_history.old"
-    [ -f "$ZDOTDIR/.zsh_history" ] && mv "$ZDOTDIR/.zsh_history" "$HOME"
-    rm -rf "$ZDOTDIR" "$HOME/.zshenv" "$HOME/.cache/p10k"* \
+    rm -rf "$HOME/.zshenv" "$HOME/.zshrc"* \
       "$HOME/.cache/zinit" "$HOME/.local/share/zinit" \
-      "$HOME/.cache/starship" "$HOME/.cache/gitstatus"
+      "$HOME/.cache/gitstatus" "$HOME/.cache/zsh/"
   fi
 }
 
