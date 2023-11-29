00e885283b7429860ebbc58a991837348f399fde
Warbacon <joaquinguerratocino@gmail.com>
2023-11-29 14:54:56 +0100

    fix: remove leftover * in uninstaller

--- uninstall.sh
+++ uninstall.sh
@@ -6,9 +6,9 @@ main() {
   printf "Do you want to remove all the Zunder files? [y/N]: "
   read -r prompt
   if [ "$prompt" = "y" ] || [ "$prompt" = "Y" ]; then
-    rm -rf "$HOME/.zshenv" "$HOME/.zshrc"* \
+    rm -rf "$HOME/.zshenv" "$HOME/.zshrc" \
       "$HOME/.cache/zinit" "$HOME/.local/share/zinit" \
-      "$HOME/.cache/gitstatus" "$HOME/.cache/zsh/"
+      "$HOME/.cache/gitstatus" "$HOME/.cache/zsh"
   fi
 }
 
