943264688a58b0115c92ee9d12a198a1555347d1
Joaquín Guerra <45147327+warbacon@users.noreply.github.com>
2024-09-27 00:37:23 +0200

    feat: spaceship improvements

--- config/spaceship.zsh
+++ config/spaceship.zsh
@@ -1,11 +1,12 @@
 SPACESHIP_PROMPT_ORDER=(
-  dir            # Current directory section
-  git            # Git section (git_branch + git_status)
-  venv           # virtualenv section
-  exec_time      # Execution time
-  line_sep       # Line break
-  jobs           # Background jobs indicator
-  char           # Prompt character
+    dir            # Current directory section
+    git            # Git section (git_branch + git_status)
+    venv           # virtualenv section
+    exec_time      # Execution time
+    async          # Async jobs indicator
+    line_sep       # Line break
+    jobs           # Background jobs indicator
+    char           # Prompt character
 )
 
 SPACESHIP_PROMPT_ADD_NEWLINE=false
@@ -13,6 +14,12 @@ _spaceship_add_newline() {
     [ -z "$_should_add_newline" ] && _should_add_newline=true || echo
 }
 precmd_functions+=(_spaceship_add_newline)
+
+SPACESHIP_DIR_LOCK_SYMBOL=" 󰌾"
+SPACESHIP_DIR_TRUNC=0
+
+SPACESHIP_VENV_PREFIX="using "
+SPACESHIP_VENV_SYMBOL=" "
 
 SPACESHIP_CHAR_COLOR_SUCCESS="reset"
 if [[ "$TERM" = "linux" ]]; then
@@ -22,8 +29,3 @@ else
     SPACESHIP_CHAR_SYMBOL_SECONDARY="· "
     SPACESHIP_CHAR_COLOR_SECONDARY=8
 fi
-
-SPACESHIP_DIR_LOCK_SYMBOL=" 󰌾"
-
-SPACESHIP_VENV_PREFIX="using "
-SPACESHIP_VENV_SYMBOL=" "
